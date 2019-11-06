b218f763d8edd543eb1b3a59117dd410d754e1ec
wolfene2001 <wolfene2001@yahoo.com>
2019-11-06 14:16:36 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,7 +1,8 @@
-def prime?(integer)
-  (1...integer).each do |n|
-    if integer / [1,integer] == 1 || integer
-      return true
+def prime?(n)
+  if n <= 1
+    return false
+  else (2..n-1).to_a.all? do |integer|
+    n % integer != 0
     end
-    false
+  end
 end
